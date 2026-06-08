5984c83f7937dc8edde1dd467147f54b37336ece
GuruChan05 <ajahguru11@gmail.com>
2026-06-08 21:23:58 +0530

    Create news.py

---
+++ news.py
@@ -0,0 +1,26 @@
+import requests
+from config import NEWS_API_KEY
+
+
+def get_news(category):
+
+    url = (
+        "https://newsapi.org/v2/top-headlines"
+        f"?category={category}"
+        "&country=us"
+        f"&apiKey={NEWS_API_KEY}"
+    )
+
+    response = requests.get(url, timeout=30)
+
+    data = response.json()
+
+    articles = data.get("articles", [])[:5]
+
+    news_text = ""
+
+    for article in articles:
+        title = article.get("title", "")
+        news_text += f"• {title}\n"
+
+    return news_text
